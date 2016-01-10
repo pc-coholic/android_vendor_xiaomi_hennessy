5e8998b56a2932f58aedd731c69a9ab4bfdb8a99
Lok Jian Ming <jianminglok@hotmail.com>
2016-01-10 12:45:22 +0800

    Update hennessy-vendor.mk

--- hennessy-vendor.mk
+++ hennessy-vendor.mk
@@ -11,6 +11,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/xiaomi/hermes/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/xiaomi/hennessy/overlay
 
-$(call inherit-product, vendor/xiaomi/hermes/hermes-vendor-blobs.mk)
+$(call inherit-product, vendor/xiaomi/hennessy/hennessy-vendor-blobs.mk)
